21ee80fcdf60919277691f647405e1ece37721a7
pat-jpnk <jpnksoftware@protonmail.com>
2020-06-08 03:54:58 +0200

    First commit, unicode hurts my soul

---
+++ functions.js
@@ -0,0 +1,35 @@
+const removeDups = xs => {
+ 
+  const collect = (r, x) => {
+    if (!r.seen[x]) {
+      r.result.push(x);
+      r.seen[x] = true;
+    }
+    return r;
+  };
+
+  return xs.reduce(collect, { seen: {}, result: [] }).result;
+};
+
+
+
+const getPhrases = function(lyrics){
+  lyrics = lyrics.trim();
+  lyrics = lyrics.replace(/\t|\.|\,/gm,"");
+  //lyrics = lyrics.replace(/\s/gm," ");
+  lyrics = lyrics.replace(/[\u00A0\u1680\u180e\u2000-\u2009\u200a\u200b\u202f\u205f\u3000\u0020]/g," ")
+  lyrics = lyrics.split(/\n/gm);
+  return removeDups(lyrics);
+
+}
+
+const getTerms = function(lyrics){
+  lyrics = lyrics.trim();
+  lyrics = lyrics.replace(/\n|\r\n/gm," ");
+  lyrics = lyrics.replace(/\?|\!|\.|\,/gm,"")
+  lyrics = lyrics.replace(/\s+/gm," ");
+  lyrics = lyrics.replace(/[\u00A0\u1680\u180e\u2000-\u2009\u200a\u200b\u202f\u205f\u3000\u0020]/g," ")
+  terms = lyrics.split(/(\s+)/gm);
+  console.log(terms.indexOf(" "))
+  return removeDups(terms)
+}
